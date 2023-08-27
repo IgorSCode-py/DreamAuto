7c19db1706f64aa7ead39d5c6f99b9b226f7c9c1
Igor Shlepanov <ishlepanov@gmail.com>
2023-08-27 20:02:53 +0400

    test: add JEST tests for carSearchForm, carTile, SampleDataImporter components

--- force-app/main/default/lwc/carTile/carTile.js
+++ force-app/main/default/lwc/carTile/carTile.js
@@ -5,23 +5,28 @@ const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper';
 
 export default class CarTile extends LightningElement {
     @api car;
-    @api selectedCarId;
+    @api 
+    selectedCarId;
     
     // dynamically setting the background image for the picture
     get backgroundStyle() { 
-        return 'background-image:url('+ this.car.Picture__c +')';
+        return `background-image:url(${this.car.Picture__c})`;
     }
     
     // if car is selected is selected set the tile class
-    get tileClass() { 
-        if (this.car.Id == this.selectedCarId) {
+    get tileClass() {
+         
+        
+        if (this.car.Id === this.selectedCarId) {
             return TILE_WRAPPER_SELECTED_CLASS;
         }
         return TILE_WRAPPER_UNSELECTED_CLASS;
+        
     }
     
     // Fires event with the Id of the car that has been selected.
     selectCar() { 
+        // eslint-disable-next-line @lwc/lwc/no-api-reassignments
         this.selectedCarId = this.car.Id;
         const carselect = new CustomEvent('carselect', {
             detail: {
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
+
 module.exports = {
     ...jestConfig,
+    moduleNameMapper: {
+        '^lightning/platformShowToastEvent$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent',
+        '^lightning/messageService$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/messageService',
+        '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex'
+    },
+    setupFilesAfterEnv,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
